148e213b8b62e5910843e7edf13769d4d217f5e0
amitrajfintech-star <amitrajfintech@gmail.com>
2026-02-23 10:55:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2867
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .AppleSystemUIFontMonospaced-Regular;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww25400\viewh15280\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec0 \{\
+    "liveServer.settings.port": 5501\
+\}}
